9369687aecb5330902cf06ea7256eac63684a3a8
asd890303 <asd890303@gmail.com>
2015-03-29 17:19:10 +0800

    0329 test

---
+++ WebApplication1/WebForm4.aspx
@@ -0,0 +1,65 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm4.aspx.cs" Inherits="WebApplication1.WebForm4" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <title>sql</title>
+      
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:TextBox ID="txtSearch" runat="server"></asp:TextBox>
+            <asp:Button ID="btnSearch" runat="server" OnClick="btnSearch_Click" Text="查詢" />
+            <br />
+            <asp:GridView ID="gvResult" runat="server" BackColor="White" BorderColor="#CCCCCC" BorderStyle="None" BorderWidth="1px" CellPadding="3" Width="464px">
+                <FooterStyle BackColor="White" ForeColor="#000066" />
+                <HeaderStyle BackColor="#006699" Font-Bold="True" ForeColor="White" />
+                <PagerStyle BackColor="White" ForeColor="#000066" HorizontalAlign="Left" />
+                <RowStyle ForeColor="#000066" />
+                <SelectedRowStyle BackColor="#669999" Font-Bold="True" ForeColor="White" />
+                <SortedAscendingCellStyle BackColor="#F1F1F1" />
+                <SortedAscendingHeaderStyle BackColor="#007DBB" />
+                <SortedDescendingCellStyle BackColor="#CAC9C9" />
+                <SortedDescendingHeaderStyle BackColor="#00547E" />
+            </asp:GridView>
+          
+            <hr />
+            <div>
+                <p>
+                    StudentID:<asp:TextBox ID="txtID" runat="server"></asp:TextBox>
+                </p>
+                <p>
+                    StudentName:<asp:TextBox ID="txtName" runat="server"></asp:TextBox>
+                </p>
+                <p>
+                    StudentAge:<asp:TextBox ID="txtAge" runat="server"></asp:TextBox>
+                </p>
+                <asp:Button ID="btnAdd" runat="server" OnClick="btnAdd_Click" Text="新增" />
+
+            </div>
+            <hr />
+            <br />
+            <p>
+                StudentID:<asp:TextBox ID="txtE_ID" runat="server"></asp:TextBox>
+            </p>
+            <p>
+                StudentName:<asp:TextBox ID="txtE_Name" runat="server"></asp:TextBox>
+            </p>
+            <p>
+                StudentAge:<asp:TextBox ID="txtE_Age" runat="server"></asp:TextBox>
+            </p>
+            <asp:Button ID="btnEdit" runat="server" OnClick="btnEdit_Click" Text="修改" />
+            <hr />
+            <div>
+                <p>
+                    StudentID:<asp:TextBox ID="delid" runat="server"></asp:TextBox>
+                </p>
+                <asp:Button ID="Button1" runat="server" OnClick="btndel_Click" Text="刪除" />
+            </div>
+        </div>
+    </form>
+</body>
+</html>
---
+++ WebApplication1/WebForm4.aspx.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebApplication1
+{
+    /// <summary>
+    /// ExecuteReader, ExecuteNonQuery, ExecuteScalar
+    /// </summary>
+    public partial class WebForm4 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            btnSearch_Click(null, null);
+        }
+
+        /// <summary>
+        /// ExecuteReader
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        protected void btnSearch_Click(object sender, EventArgs e)
+        {
+            using (SqlConnection cn = new SqlConnection(ConfigurationManager.ConnectionStrings["ADODBConnectionString2"].ConnectionString))
+            {
+                cn.Open();
+                using (SqlCommand cmd = cn.CreateCommand())
+                {
+                    cmd.CommandText = "select * from student where student_name like @name";
+                    cmd.Parameters.Add(new SqlParameter("@name", "%" + txtSearch.Text + "%"));
+                    using (SqlDataReader dr = cmd.ExecuteReader())
+                    {
+                        DataTable dt = new DataTable();
+                        dt.Load(dr);
+                        gvResult.DataSource = dt;
+                        gvResult.DataBind();
+                        dr.Close();
+                    }
+                }
+            }
+        }
+
+        //ExecuteScalar
+        protected void btnAdd_Click(object sender, EventArgs e)
+        {
+            string sql = @"INSERT INTO [dbo].[student]
+           ([student_name]
+           ,[student_id]
+           ,[student_age])
+     VALUES
+                               (@Name
+                               ,@id,@age);";
+            using (SqlConnection cn = new SqlConnection(ConfigurationManager.ConnectionStrings["ADODBConnectionString2"].ConnectionString))
+            {
+                cn.Open();
+                using (SqlCommand cmd = cn.CreateCommand())
+                {
+                    cmd.CommandText = sql;
+                    cmd.Parameters.Add(new SqlParameter("@Name", txtName.Text));
+                    cmd.Parameters.Add(new SqlParameter("@id", txtID.Text));
+                    cmd.Parameters.Add(new SqlParameter("@age", txtAge.Text));
+                    cmd.ExecuteNonQuery();
+                }
+                btnSearch_Click(null, null);
+            }
+        }
+
+        /// <summary>
+        /// ExecuteNonQuery
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        protected void btnEdit_Click(object sender, EventArgs e)
+        {
+            string sql = @"update [student] set [student_name] = @Name,[student_age] = @Age
+                             where [student_id] = @ID";
+            using (SqlConnection cn = new SqlConnection(ConfigurationManager.ConnectionStrings["ADODBConnectionString2"].ConnectionString))
+            {
+                cn.Open();
+                using (SqlCommand cmd = cn.CreateCommand())
+                {
+                    cmd.CommandText = sql;
+                    cmd.Parameters.Add(new SqlParameter("@ID", txtE_ID.Text));
+                    cmd.Parameters.Add(new SqlParameter("@Name", txtE_Name.Text));
+                    cmd.Parameters.Add(new SqlParameter("@Age", txtE_Age.Text));
+
+                    cmd.ExecuteNonQuery();
+                }
+                btnSearch_Click(null, null);
+            }
+        }
+
+        protected void btndel_Click(object sender, EventArgs e)
+        {
+            using (SqlConnection cn = new SqlConnection(ConfigurationManager.ConnectionStrings["ADODBConnectionString2"].ConnectionString))
+            {
+                using (SqlCommand cmd = cn.CreateCommand())
+                {
+                    cmd.CommandText = "select * from [student]";
+
+                    using (SqlDataAdapter da = new SqlDataAdapter(cmd))
+                    {
+                        //自動產生Insert, Update, DeleteCommand
+                        SqlCommandBuilder builder = new SqlCommandBuilder(da);
+                        //builder.GetInsertCommand()
+                        //builder.GetUpdateCommand()
+                        //builder.GetDeleteCommand()
+
+                        DataSet ds = new DataSet();
+                        ds.Clear();
+                        da.Fill(ds);
+                        DataTable dt = ds.Tables[0];
+                        DataRow dr = dt.Select(string.Format("student_id = {0}", delid.Text)).First();
+                        dr.Delete();
+                        da.Update(dt);
+
+                        btnSearch_Click(null, null);
+                    }
+                }
+            }
+        }
+    }
+}
